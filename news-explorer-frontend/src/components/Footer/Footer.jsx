86139ab34c64b1cbbecf9992c45fd5613e03a3ed
ValerieTsan <valeriejean.tsan@gmail.com>
2025-05-18 19:51:03 -0400

    Fix footer layout on mobile view

--- news-explorer-frontend/src/components/Footer/Footer.jsx
+++ news-explorer-frontend/src/components/Footer/Footer.jsx
@@ -6,40 +6,32 @@ function Footer() {
   return (
     <footer className="footer">
       <p className="footer__text">© 2025 Supersite, Powered by News API</p>
-      <div className="footer__social">
-        <nav className="footer__nav">
-          <a href="/" className="footer__link">
-            Home
-          </a>
-          <a
-            href="https://tripleten.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="footer__link"
-          >
-            Tripleten
-          </a>
-        </nav>
-        <div className="footer__media">
-          <a href="https://github.com" target="_blank">
-            <img
-              className="footer__github-icon"
-              alt="GitHub"
-              src={githubLink}
-            />
-          </a>
-          <a
-            href="https://facebook.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              className="footer__fb-icon"
-              alt="Facebook"
-              src={facebookLink}
-            />
-          </a>
-        </div>
+      {/* <div className="footer__social"> */}
+      <nav className="footer__nav">
+        <a href="/" className="footer__link-home">
+          Home
+        </a>
+        <a
+          href="https://tripleten.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footer__link-tripleten"
+        >
+          Tripleten
+        </a>
+      </nav>
+      <div className="footer__media">
+        <a href="https://github.com" target="_blank">
+          <img className="footer__github-icon" alt="GitHub" src={githubLink} />
+        </a>
+        <a
+          href="https://facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="footer__fb-icon" alt="Facebook" src={facebookLink} />
+        </a>
+        {/* </div> */}
       </div>
     </footer>
   );
